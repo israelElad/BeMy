a2e3182ac22c82d3b04dd90ac31211307e35b021
maoragai <maoragai@gmail.com>
2019-06-05 15:46:14 +0300

    volunteer class gson in gradle

---
+++ app/src/main/java/com/example/bemyapp/Volunteer.java
@@ -0,0 +1,71 @@
+package com.example.bemyapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Volunteer {
+    //personal info
+    private String name;
+    private String phoneNumber;
+    //help info
+    private TreeMap avilableFor;
+    private ArrayList<Date> avilableWhen;
+    //location info
+    private String city;
+
+
+
+    public Volunteer(String name, String phoneNumber, TreeMap avilableFor, ArrayList<Date> avilableWhen, String city) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.avilableFor = avilableFor;
+        this.avilableWhen = avilableWhen;
+        this.city = city;
+        
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public TreeMap getAvilableFor() {
+        return avilableFor;
+    }
+
+    public void setAvilableFor(TreeMap avilableFor) {
+        this.avilableFor = avilableFor;
+    }
+
+    public ArrayList<Date> getAvilableWhen() {
+        return avilableWhen;
+    }
+
+    public void setAvilableWhen(ArrayList<Date> avilableWhen) {
+        this.avilableWhen = avilableWhen;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
