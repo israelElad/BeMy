4bdb7f30cea2eef5ae611244e6691c537da14014
Yael Magid <yael.magid@live.biu.ac.il>
2019-06-05 20:36:37 +0300

    update

--- app/src/main/java/com/example/bemyapp/MyDate.java
+++
@@ -1,8 +0,0 @@
-package com.example.bemyapp;
-
-public class MyDate {
-    private String day;
-    //hour format hh:mm:ss
-    private String hour;
-
-}
--- app/src/main/java/com/example/bemyapp/Seek.java
+++ app/src/main/java/com/example/bemyapp/Seek.java
@@ -2,21 +2,35 @@ package com.example.bemyapp;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public class Seek {
 
     boolean isAvaliableInTime(Volunteer volunteer){
+        if (volunteer.getBusy()) {
+            return false;
+        }
         Date now = new Date();
+        Map<Date, Date> schedual =  volunteer.getAvailableWhen();
+        for (Map.Entry<Date, Date> entry : schedual.entrySet()) {
+            if (entry.getKey().before(now) && entry.getValue().after(now)) {
+                return true;
+            }
+        }
         return false;
     }
 
-    public Volunteer seekVolunteerForLegs() {
+    public Volunteer seekVolunteerForLegs(String city) {
         List<Volunteer> volunteers = Info.getInstance().getVolunteersList();
         for (int i = 0; i < volunteers.size(); i++) {
-            if ((volunteers.get(i).getAvailableFor().get("call") == null) &&
-                    volunteers.get(i).getAvailableFor().get("call") &&
+            if(volunteers.get(i).getAvailableFor().get("call") &&
                     volunteers.get(i).getAvailableFor().get("mission")) {
-                //TODO: BLA
+                if(volunteers.get(i).getCity().equals(city)) {
+                    if(isAvaliableInTime(volunteers.get(i))){
+                        volunteers.get(i).setBusy(true);
+                        return volunteers.get(i);
+                    }
+                }
             }
         }
         return null;
