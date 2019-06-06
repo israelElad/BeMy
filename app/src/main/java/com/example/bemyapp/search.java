922b4b4e57769c38ba7f25cc8da57d62d345c256
Yael Magid <yael.magid@live.biu.ac.il>
2019-06-06 05:40:51 +0300

    update

--- app/src/main/java/com/example/bemyapp/search.java
+++ app/src/main/java/com/example/bemyapp/search.java
@@ -1,6 +1,8 @@
 package com.example.bemyapp;
 
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -18,21 +20,72 @@ public class search extends AppCompatActivity {
         List<Volunteer> volunteerList;
         if(beMyWhat.equals("Mouth")){
             volunteerList=searchVolunteer.getVolunteersForEarsOrMouth();
-            
-            Intent intent = new Intent(search.this, chooseContactWay.class);
-            startActivity(intent);
+            if (volunteerList != null) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel1")
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Someone is looking for you")
+                        .setContentText("press OK to continue")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("press OK to continue"))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, builder.build());
+                Intent intent = new Intent(search.this, chooseContactWay.class);
+                startActivity(intent);
+            }
         }else if(beMyWhat.equals("Ear")){
             volunteerList=searchVolunteer.getVolunteersForEarsOrMouth();
-            Intent intent = new Intent(search.this, chooseContactWay.class);
-            startActivity(intent);
+            if (volunteerList != null) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel1")
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Someone is looking for you")
+                        .setContentText("press OK to continue")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("press OK to continue"))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, builder.build());
+                Intent intent = new Intent(search.this, chooseContactWay.class);
+                startActivity(intent);
+            }
         }else if(beMyWhat.equals("Leg")){
             volunteerList=searchVolunteer.getVolunteersForLegs("Jerusalem");
-            Intent intent = new Intent(search.this, chooseContactWay.class);
-            startActivity(intent);
+            if (volunteerList != null) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel1")
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Someone is looking for you")
+                        .setContentText("press OK to continue")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("press OK to continue"))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, builder.build());
+                Intent intent = new Intent(search.this, chooseContactWay.class);
+                startActivity(intent);
+            }
         }else if(beMyWhat.equals("Friend")){
-            volunteerList=searchVolunteer.getVolunteersForFriend("Jerusalem");
-            Intent intent = new Intent(search.this, chooseContactWay.class);
-            startActivity(intent);
+            volunteerList=searchVolunteer.getVolunteersForFriend("Tel-Aviv");
+            if (volunteerList != null) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel1")
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Someone is looking for you")
+                        .setContentText("press OK to continue")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("press OK to continue"))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, builder.build());
+                Intent intent = new Intent(search.this, chooseContactWay.class);
+                startActivity(intent);
+            }
         }else{
             System.out.println("beMy");
         }
