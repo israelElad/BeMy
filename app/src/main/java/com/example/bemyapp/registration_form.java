3f97a14a6cee7eb514f3ba2688b66776259d0c39
Elad Israel <elad.israel@live.biu.ac.il>
2019-06-06 03:38:31 +0300

    Merged 3:30 - Views

--- app/src/main/java/com/example/bemyapp/registration_form.java
+++ app/src/main/java/com/example/bemyapp/registration_form.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class registration_form extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/bemyapp/search.java
+++ app/src/main/java/com/example/bemyapp/search.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class search extends AppCompatActivity {
 
     @Override
@@ -12,16 +14,23 @@ public class search extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         String beMyWhat=getIntent().getStringExtra("BeMy");
         //todo: change to Seek
+        SearchVolunteer searchVolunteer=new SearchVolunteer();
+        List<Volunteer> volunteerList;
         if(beMyWhat.equals("Mouth")){
+            volunteerList=searchVolunteer.getVolunteersForEarsOrMouth();
+            
             Intent intent = new Intent(search.this, chooseContactWay.class);
             startActivity(intent);
         }else if(beMyWhat.equals("Ear")){
+            volunteerList=searchVolunteer.getVolunteersForEarsOrMouth();
             Intent intent = new Intent(search.this, chooseContactWay.class);
             startActivity(intent);
         }else if(beMyWhat.equals("Leg")){
+            volunteerList=searchVolunteer.getVolunteersForLegs("Jerusalem");
             Intent intent = new Intent(search.this, chooseContactWay.class);
             startActivity(intent);
         }else if(beMyWhat.equals("Friend")){
+            volunteerList=searchVolunteer.getVolunteersForFriend("Jerusalem");
             Intent intent = new Intent(search.this, chooseContactWay.class);
             startActivity(intent);
         }else{
