28c9b68c150c4a37e2af5ed5c01d88961857e74e
Narkis Kremizi <narkis.kremizi@live.biu.ac.il>
2019-06-06 06:08:54 +0300

    now work

--- app/src/main/java/com/example/bemyapp/MainActivity.java
+++ app/src/main/java/com/example/bemyapp/MainActivity.java
@@ -1,68 +1,37 @@
 package com.example.bemyapp;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    VolunteerDataBase volunteerDataBase = new VolunteerDataBase(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Info.getInstance().setVolunteersList(volunteerDataBase.loadFromFile());
         super.onCreate(savedInstanceState);
-        createNotificationChannel();
         setContentView(R.layout.activity_main);
         VolunteerDataBase volunteerDataBase = new VolunteerDataBase(this);
-        Map<String, Boolean> availableFor = new HashMap<>();
-        availableFor.put("callOrMessage", true);
-        availableFor.put("Mission", true);
-        Map<Date, Date> availableWhen = new HashMap<>();
-        availableWhen.put(volunteerDataBase.toDate("Thu Jun 06 01:26:00 GMT 2019"), volunteerDataBase.toDate("Thu Jun 06 02:40:00 GMT 2019"));
-        Info.getInstance().addVolunteer("Yael", "0544736473", availableFor, "Tel-Aviv", availableWhen);
-        /*
-        // narkis's check
-        VolunteerDataBase volunteerDataBase = new VolunteerDataBase(this);
-        Map<String, Boolean> availableFor = new HashMap<>();
-        availableFor.put("callOrMessage", true);
-        availableFor.put("Mission", true);
-        Map<Date, Date> availableWhen = new HashMap<>();
-        availableWhen.put(volunteerDataBase.toDate("Thu Jun 06 01:26:00 GMT 2019"), volunteerDataBase.toDate("Thu Jun 06 01:40:00 GMT 2019"));
-        Info.getInstance().addVolunteer("Yael", "0544736473", availableFor, "Tel-Aviv", availableWhen);
-        //volunteerDataBase.WriteToFile(this);
-        //Info.getInstance().setVolunteersList(volunteerDataBase.loadFromFile(this));
-        List<Volunteer> nlist = new SearchVolunteer().getVolunteersForFriend("Tel-Aviv");
-        */
-    }
+        Info.getInstance().setVolunteersList(volunteerDataBase.loadFromFile());
 
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "approveNotification";
-            String description = "approve notification";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("channel1", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        // narkis's check
+        /*VolunteerDataBase volunteerDataBase = new VolunteerDataBase(this);
+        Map<String, Boolean> availableFor = new HashMap<>();
+        availableFor.put("callOrMessage", false);
+        availableFor.put("Mission", false);
+        Map<Date, Date> availableWhen = new HashMap<>();
+        availableWhen.put(volunteerDataBase.toDate("Wed Oct 16 00:00:00 PST 2013"), volunteerDataBase.toDate("Wed Oct 16 00:06:00 PST 2013"));
+        Info.getInstance().addVolunteer("Yael", "0544736473", availableFor, "Tel-Aviv", availableWhen);
+        volunteerDataBase.WriteToFile(this);
+        Info.getInstance().setVolunteersList(volunteerDataBase.loadFromFile(this));*/
     }
 
     @Override
-    public void onStop() {
-        //volunteerDataBase.WriteToFile();
-        super.onStop();
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     public void beMyClick(View view)
