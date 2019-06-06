d932ce464ca22bc7d380a7287559bc8831bbeebe
maoragai <maoragai@gmail.com>
2019-06-06 08:35:48 +0300

    beFor updated mainActivity updated registration form updated

--- app/src/main/java/com/example/bemyapp/registration_form.java
+++ app/src/main/java/com/example/bemyapp/registration_form.java
@@ -39,7 +39,7 @@ public class registration_form extends AppCompatActivity {
         Map<String,Boolean> availableFor =new TreeMap<String,Boolean>();
         availableFor.put("callOrMessage",callAndMessage);
         availableFor.put("Mission",errand);
-
+try {
     Map<Date, Date> availableWhen = new HashMap<>();
     availableWhen.put(volunteerDataBase.toDate("Thu Jun 06 01:26:00 GMT 2019"), volunteerDataBase.toDate("Thu Jun 06 10:40:00 GMT 2019"));
     availableWhen.put(volunteerDataBase.toDate("Thu Jun 07 01:26:00 GMT 2019"), volunteerDataBase.toDate("Thu Jun 07 10:40:00 GMT 2019"));
@@ -48,9 +48,9 @@ public class registration_form extends AppCompatActivity {
     Info.getInstance().addVolunteer(volunteer);
     Intent intent=new Intent(registration_form.this , register.class);
     startActivity(intent);
+}catch(Exception e){
 
-
-    }
+    }}
 
     public String viewToString(View view){
         EditText editText = (EditText) view;
