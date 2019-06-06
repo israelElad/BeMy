b305e3ae10bba2f20c21a750b5668bc77a6cf1d8
Elad Israel <elad.israel@live.biu.ac.il>
2019-06-06 04:35:26 +0300

    Merged 4:30 - Views

--- app/src/main/java/com/example/bemyapp/MainActivity.java
+++ app/src/main/java/com/example/bemyapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void beForClick(View view)
     {
-        Intent intent = new Intent(MainActivity.this, ---------.class);
+        Intent intent = new Intent(MainActivity.this, registration_form.class);
         startActivity(intent);
     }
 }
